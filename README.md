fea98fef97135132037647632a20f7a2847079ad
avillegas1717 <134570292+avillegas1717@users.noreply.github.com>
2024-04-17 11:21:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Intro-to-JavaScript
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/avillegas1717/Intro-to-JavaScript)
---
+++ script.js
@@ -0,0 +1,12 @@
+// Define two numeric variables
+const dog = 5;
+const puppy = 25;
+
+// Set a new variable to determine if twice the first numeric variable is greater than the second numeric variable from above.
+const bone = dog * 2 > puppy;
+
+// Output the comparison.
+document.getElementById('math').innerHTML = bone;
+
+// Practice types and equality
+document.getElementById('type').innerHTML = typeof dog;
